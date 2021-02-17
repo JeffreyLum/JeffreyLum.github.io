db04a5984d2ed8fd221bccd1db77d60f01167cfc
JeffreyLum <JeffreyLum@users.noreply.github.com>
2021-02-16 20:40:38 -0800

    Create main page.js

---
+++ assets/js/main page.js
@@ -0,0 +1,17 @@
+
+//Scrolls to the about section when "About" is clicked
+function about() {
+    var element = document.getElementById("AboutSection");
+    element.scrollIntoView();
+}
+
+//Scrolls to the contact me secion when "Contact" is clicked
+function contact() {
+    var element = document.getElementById("ContentSection");
+    element.scrollIntoView();
+}
+
+function projects() {
+    var element = document.getElementById("");
+    element.scrollIntoView();
+}
