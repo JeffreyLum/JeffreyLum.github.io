c7951c3ba8fea54494d57d1190a0cf64cbe2b669
JeffreyLum <JeffreyLum@users.noreply.github.com>
2021-02-23 00:30:31 -0800

    Update main page.js
    added in javascript for scroll back to top

--- assets/js/main page.js
+++ assets/js/main page.js
@@ -1,17 +1,40 @@
 
 //Scrolls to the about section when "About" is clicked
 function about() {
-    var element = document.getElementById("AboutSection");
+    let element = document.getElementById("AboutSection");
     element.scrollIntoView();
 }
 
 //Scrolls to the contact me secion when "Contact" is clicked
 function contact() {
-    var element = document.getElementById("ContentSection");
+    let element = document.getElementById("ContentSection");
     element.scrollIntoView();
 }
 
 function projects() {
-    var element = document.getElementById("");
+    let element = document.getElementById("");
     element.scrollIntoView();
+}
+
+window.onload = function () {
+    let button = document.getElementById("goTop");
+
+    window.onscroll = function () {
+        scroll()
+    }
+
+    function scroll() {
+        if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+            button.style.display = "block"
+        }
+        else {
+            button.style.display = "none"
+        }
+    }
+}
+
+function goToTop() {
+
+    document.body.scrollTop = 0
+    document.documentElement.scrollTop = 0
 }
