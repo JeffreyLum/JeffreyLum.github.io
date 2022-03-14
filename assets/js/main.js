7b05fc37d28b76535d6e763488bf83c7086c331a
Jeffrey Lum <jeffreylum81@gmail.com>
2022-03-14 00:42:05 -0700

    Temporarily removed the three.js stuff

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,43 +1,46 @@
 let scene, camera, renderer, cube;
 
 function init() {
-    scene = new THREE.Scene();
+	scene = new THREE.Scene();
 
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+	camera = new THREE.PerspectiveCamera(
+		75,
+		window.innerWidth / window.innerHeight,
+		0.1,
+		1000
+	);
 
-    renderer = new THREE.WebGLRenderer({ antialias: true });
+	renderer = new THREE.WebGLRenderer({ antialias: true });
 
-    renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.setSize(window.innerWidth, window.innerHeight);
 
-    document.body.appendChild(renderer.domElement);
+	document.body.appendChild(renderer.domElement);
 
-    const geometry = new THREE.BoxGeometry(2, 2, 2);
-    // const material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
-    const texture = new THREE.TextureLoader().load('assets/textures/chiri.jpg')
-    const material = new THREE.MeshBasicMaterial({ map: texture });
-    cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+	const geometry = new THREE.BoxGeometry(2, 2, 2);
+	// const material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+	const texture = new THREE.TextureLoader().load('assets/textures/chiri.jpg');
+	const material = new THREE.MeshBasicMaterial({ map: texture });
+	cube = new THREE.Mesh(geometry, material);
+	scene.add(cube);
 
-    camera.position.z = 5
+	camera.position.z = 5;
 }
 
-
 function animate() {
-    requestAnimationFrame(animate);
+	requestAnimationFrame(animate);
 
-    cube.rotation.x += 0.002;
-    cube.rotation.y += 0.003;
+	cube.rotation.x += 0.002;
+	cube.rotation.y += 0.003;
 
-    renderer.render(scene, camera);
+	renderer.render(scene, camera);
 }
 
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-window.addEventListener('resize', onWindowResize, false)
+window.addEventListener('resize', onWindowResize, false);
 init();
 animate();
