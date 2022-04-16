b754ef744db5dc3e06c09b223c1ab6a1e30e97b3
Jeffrey Lum <jeffreylum81@gmail.com>
2022-04-16 12:33:20 -0700

    ...

--- assets/js/main page.js
+++ assets/js/main page.js
@@ -1,40 +1,40 @@
-
 //Scrolls to the about section when "About" is clicked
 function about() {
-    let element = document.getElementById("AboutSection");
-    element.scrollIntoView();
+	let element = document.getElementById('AboutSection');
+	element.scrollIntoView();
 }
 
 //Scrolls to the contact me secion when "Contact" is clicked
 function contact() {
-    let element = document.getElementById("ContentSection");
-    element.scrollIntoView();
+	let element = document.getElementById('ContentSection');
+	element.scrollIntoView();
 }
 
 function projects() {
-    let element = document.getElementById("");
-    element.scrollIntoView();
+	let element = document.getElementById('');
+	element.scrollIntoView();
 }
 
 window.onload = function () {
-    let button = document.getElementById("goTop");
+	let button = document.getElementById('goTop');
 
-    window.onscroll = function () {
-        scroll()
-    }
+	window.onscroll = function () {
+		scroll();
+	};
 
-    function scroll() {
-        if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
-            button.style.display = "block"
-        }
-        else {
-            button.style.display = "none"
-        }
-    }
-}
+	function scroll() {
+		if (
+			document.body.scrollTop > 800 ||
+			document.documentElement.scrollTop > 800
+		) {
+			button.style.display = 'block';
+		} else {
+			button.style.display = 'none';
+		}
+	}
+};
 
 function goToTop() {
-
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
 }
--- assets/js/nameType.js
+++ assets/js/nameType.js
@@ -1,32 +1,31 @@
-let getName = document.getElementById('namelink')
+let getName = document.getElementById('namelink');
 let animation = [
-    { n: '|', ms: 300 },
-    { n: 'J|', ms: 200 },
-    { n: 'Jef|', ms: 200 },
-    { n: 'Jeff|', ms: 200 },
-    { n: 'Jeffr|', ms: 200 },
-    { n: 'Jeffre|', ms: 200 },
-    { n: 'Jeffrey|', ms: 200 },
-    { n: 'Jeffrey |', ms: 200 },
-    { n: 'Jeffrey L|', ms: 200 },
-    { n: 'Jeffrey Lu|', ms: 200 },
-    { n: 'Jeffrey Lum' },
-]
+	{ n: '|', ms: 300 },
+	{ n: 'J|', ms: 200 },
+	{ n: 'Jef|', ms: 200 },
+	{ n: 'Jeff|', ms: 200 },
+	{ n: 'Jeffr|', ms: 200 },
+	{ n: 'Jeffre|', ms: 200 },
+	{ n: 'Jeffrey|', ms: 200 },
+	{ n: 'Jeffrey |', ms: 200 },
+	{ n: 'Jeffrey L|', ms: 200 },
+	{ n: 'Jeffrey Lu|', ms: 200 },
+	{ n: 'Jeffrey Lum' },
+];
 
-let stepD = 1
+let stepD = 1;
 if (window.localStorage.setpD) {
-    stepD = window.localStorage.stepD
+	stepD = window.localStorage.stepD;
 }
 
-let i = 0
+let i = 0;
 let update = () => {
-    let step = animation[i]
-    getName.innerText = step.n
-    i++
+	let step = animation[i];
+	getName.innerText = step.n;
+	i++;
 
-    if (i < animation.length) {
-        setTimeout(update, step.ms / stepD)
-    }
-
-}
-update()
+	if (i < animation.length) {
+		setTimeout(update, step.ms / stepD);
+	}
+};
+update();
